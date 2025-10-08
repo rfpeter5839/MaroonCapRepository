022aa2ef79b2ff84cb8f2dbf44748779abc4b514
rfpeter5839 <rfpeterson@uchicago.edu>
2025-10-07 23:33:20 -0500

    Delete Maroon Cap Python Code/Maroon Cap Python Code directory

--- Maroon Cap Python Code/Maroon Cap Python Code/NumberSets.py
+++
@@ -1,4 +0,0 @@
-def sets_needed_to_win(total_games):
-    if total_games % 2 == 0 or total_games < 1:
-        raise ValueError("Total games must be a positive odd number (2n+1)")
-    return (total_games // 2) + 1
